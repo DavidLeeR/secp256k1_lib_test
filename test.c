d701433ab87da050071972b5e02a5c6ee2544d3e
David Lee Ramirez <lee@leapwithalice.io>
2019-02-08 15:40:59 -0800

    Update test.c

--- test.c
+++ test.c
@@ -8,8 +8,14 @@ int main()
 
     /*the following 2 variables not currently working correctly, they need to be in either uint256 from (Bitcoin) or h256 form (Ethereum)
     This requires that we include more source code from Ethereum C++ repo*/
-    unsigned char myMessageHash[65] = "357632a869c2a27ecf8b9fdb74bd0f21489e99acd52ef67f1c0e0b2bc7d9bae6";
-    unsigned char myPrivateKey[65] = "402270cdf114bc39debb75ee68ca4ed61e8000bf2a2ce86394f3b8b00878b7cb";
+
+    /*according to code in tests_exhaustive.c (within go-ethereum-master) project, we should
+    be able to use this format for the key and message*/
+    secp256k1_scalar msg;
+    unsigned char myMessageHash[32], myPrivateKey[32];
+
+    secp256k1_scalar_get_b32(msg32, &msg);
+
 
     const unsigned char *myMessageHashPtr = &myMessageHash;
     const unsigned char *myPrivateKeyPtr = &myPrivateKey;
