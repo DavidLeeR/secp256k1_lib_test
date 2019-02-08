fde51bed1ffc62612f1d893985071dd8fe4298f9
David Lee Ramirez <lee@leapwithalice.io>
2019-02-08 13:05:49 -0800

    incude folder created

--- test.c
+++ test.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
-#include "secp256k1.h"
+#include "include/secp256k1.h"
 
 int main() 
 {
     secp256k1_context *myContext = secp256k1_context_create(SECP256K1_CONTEXT_SIGN);
     secp256k1_ecdsa_signature mySig;
 
+    /*the following 2 variables not currently working correctly, they need to be in either uint256 from (Bitcoin) or h256 form (Ethereum)
+    This requires that we include more source code from Ethereum C++ repo*/
+    unsigned char myMessageHash[65] = "357632a869c2a27ecf8b9fdb74bd0f21489e99acd52ef67f1c0e0b2bc7d9bae6";
+    unsigned char myPrivateKey[65] = "402270cdf114bc39debb75ee68ca4ed61e8000bf2a2ce86394f3b8b00878b7cb";
+
     const unsigned char *myMessageHashPtr = &myMessageHash;
     const unsigned char *myPrivateKeyPtr = &myPrivateKey;
-
-
-    /*the following 2 variables not currently working, they need to be in either uint256 from (Bitcoin) or h256 form (Ethereum)
-    This requires that we include more source code from Ethereum C++ repo*/
-    //unsigned char myMessageHash[65] = "357632a869c2a27ecf8b9fdb74bd0f21489e99acd52ef67f1c0e0b2bc7d9bae6";
-    //unsigned char myPrivateKey[65] = "402270cdf114bc39debb75ee68ca4ed61e8000bf2a2ce86394f3b8b00878b7cb";
 
     secp256k1_ecdsa_sign(myContext, &mySig, myMessageHashPtr, myPrivateKeyPtr, NULL, NULL);
 
