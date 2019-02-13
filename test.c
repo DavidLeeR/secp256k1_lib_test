ade5c96f144cf74e69362793dda7dabd6e54aafd
David Lee Ramirez <lee@leapwithalice.io>
2019-02-13 08:48:58 -0800

    new random generation function used for test message hash and priv key
    -provides more realistic test variables

--- test.c
+++ test.c
@@ -13,7 +13,7 @@ void random_scalar_order_test_new(secp256k1_scalar *num) {
    do {
        unsigned char b32[32];
        int overflow = 0;
-       secp256k1_rand256_test(b32);
+       secp256k1_rand256(b32);
        secp256k1_scalar_set_b32(num, b32, &overflow);
        if (overflow || secp256k1_scalar_is_zero(num)) {
            continue;
@@ -24,7 +24,6 @@ void random_scalar_order_test_new(secp256k1_scalar *num) {
 
 int main() 
 {
-    srand(time(0));
     /*a general template for this function can be found in 
     go-ethereum-master\crypto\secp256k1\libsecp256k1\src\modules\recovery\tests_impl.h
     line 150*/
@@ -33,6 +32,7 @@ int main()
     ////set to both sign and verify
     secp256k1_context *myContext = secp256k1_context_create(SECP256K1_CONTEXT_SIGN| SECP256K1_CONTEXT_VERIFY); 
     secp256k1_ecdsa_signature mySig;
+    //holds four 64 bit uints (0 to 18,446,744,073,709,551,615) in an array
     secp256k1_scalar myMessageHash, myPrivateKey;
     secp256k1_pubkey myPublicKey;
     unsigned char myMessageHash32[32], myPrivateKey32[32];
@@ -136,14 +136,15 @@ int main()
 /*
 LOG
 
-1/11/16 - does not compile, needs declarations/implementations for "secp256k1_scaler" and "secp256k1_scalar_get_b32"
-
+1/11/19 - does not compile, needs declarations/implementations for "secp256k1_scaler" and "secp256k1_scalar_get_b32"
         - compilation works after including more headers and typedef for uint128 in scalar_4_64, need to check output somehow
 
-1/12/16 - compiles and outputs signature
-
+1/12/19 - compiles and outputs signature
         - need to verify signature and customize inputs to signature function
-
         - debug statements added
+        - verify signature implemented
+        - priv/pub key creation, signing, and verification seemingly working
+
+1/13/19 - 
 
 */
